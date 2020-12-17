7536d23a3d69a392d0c3befac91ad75da56baf31
Louis Leffler <louisleffler@gmail.com>
2020-12-17 12:58:05 -0700

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,24 @@
+class Person
+
+  def name=(new_name)
+    @name = new_name
+  end
+  #writter
+  
+  def name 
+    @name 
+  end 
+  #reader
+  
+  def job=(new_job)
+    @job = new_job
+  end 
+  #writter
+  
+  def job
+    @job
+  end 
+  #reader
+  
+
+end 
