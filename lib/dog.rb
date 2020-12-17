23fe179b3fc351865353f22cfb1f7be51169dad6
Louis Leffler <louisleffler@gmail.com>
2020-12-17 12:56:17 -0700

    commit tree 39b1bc04c9cd526279b0bc491593be1325dc1e17

---
+++ lib/dog.rb
@@ -0,0 +1,23 @@
+class Dog
+
+def name=(new_name)
+  @name = new_name
+end
+#writter
+
+def name 
+  @name 
+end 
+#reader
+
+def breed=(new_breed)
+  @breed = new_breed
+end 
+#writter
+
+def breed
+  @breed
+end 
+#reader
+
+end 
